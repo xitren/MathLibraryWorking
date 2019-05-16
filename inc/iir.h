9a6991ead54dfaeae7e3de8c3b60dae5cdba2e82
denisprilutsky <denis.prilutsky.personal@gmail.com>
2019-05-16 14:45:22 +0300

    add fir

---
+++ inc/iir.h
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+#ifndef IIR_H
+#define IIR_H
+
+#include "ml_types.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct {
+    char        *name;              /* Name filter */
+    float32_t   sampl_freq_hz;      /* Sampling frequency of signals, Hz. Must be more than zero */
+    float32_t   notch_freq_hz;     /* Notch frequency */
+    float32_t   a1;     /* Not used for init */
+    float32_t   a2;     /* Not used for init */
+    float32_t   b0;     /* Not used for init */
+    float32_t   b1;     /* Not used for init */
+    float32_t   b2;     /* Not used for init */
+    float32_t   G;      /* Not used for init */
+    float32_t   x_1;    /* Not used for init */
+    float32_t   x_2;    /* Not used for init */
+    float32_t   y_1;    /* Not used for init */
+    float32_t   y_2;    /* Not used for init */
+} iir_notch_opt_t;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* IIR_H */
+
